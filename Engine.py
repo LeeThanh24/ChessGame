174db1ddd83fd8c3bc6a38fef5df0c6ceb46a9e6
ThanhBear <leethanh24003@gmail.com>
2023-04-24 20:28:28 +0700

    update pieces move

--- Engine.py
+++ Engine.py
@@ -38,3 +38,35 @@ class GameState:
 
         # To capture the movement of each piece
         self.moveLog = []
+
+    def makeMove (self,move ) :
+        self.board[move.startRow][move.startCol]="--" #we want to move so leave the square
+        self.board[move.endRow][move.endCol]=move.pieceMoved
+        self.moveLog.append(move) #log the move in order to  undo if necessary
+        self.whiteToMove=  not self.whiteToMove #swap player
+
+"""Movement of each piece"""
+
+
+class Move():
+    # the ranks are called rows (1-8) , columns are called files(A-H)
+    ranksToRows = {"1": 7, "2": 6, "3": 5, "4": 4, "5": 3, "6": 2, "7": 1, "8": 0}  # index of matrix
+    rowsToRanks = {v: k for k, v in ranksToRows.items()}
+    filesToCols = {"a": 0, "b": 1, "c": 2, "d": 3, "e": 4, "f": 5, "g": 6, "h": 7}
+    colsToFiles = {v: k for k, v in filesToCols.items()}
+
+    def __init__(self, startSq, endSq, board):
+        self.startRow = startSq[0]
+        self.startCol = startSq[1]
+        self.endRow = endSq[0]
+        self.endCol = endSq[1]
+        self.pieceMoved = board[self.startRow][self.startCol]
+        self.pieceCaptured = board[self.endRow][self.endCol]
+
+    #get the whole notation movement of a piece :example player 1 go from a6 to c6 -> a6 c6
+    def getChessNotation(self):
+        return self.getRankFile(self.startRow, self.startCol) +" "+ self.getRankFile(self.endRow, self.endCol)
+
+    # example : c6 a8 ...
+    def getRankFile(self, r, c):
+        return self.colsToFiles[c] + self.rowsToRanks[r]
--- Main.py
+++ Main.py
@@ -1,7 +1,7 @@
 """this is main driver file. It will be responsible for handling user input
 and displaying the current GameState object """
 import pygame as p
-from Engine import GameState
+import Engine
 
 WIDTH = HEIGHT = 512
 DIMENSION = 8
@@ -12,44 +12,70 @@ IMAGES = {}
 '''
 LOAD IMAGE FOR ALL PIECES
 '''
+
+
 def loadImages():
     pieces = ['bp', "bR", "bN", "bB", "bQ", "bK", "wR", "wN", "wB", "wQ", "wK", 'wp']
     for piece in pieces:
-        IMAGES[piece] = p.transform.scale(p.image.load("images/" + piece + ".png"), (SQ_SIZE, SQ_SIZE)) #Resizes the Surface to a new size, given as (width, height)
+        IMAGES[piece] = p.transform.scale(p.image.load("images/" + piece + ".png"), (
+            SQ_SIZE, SQ_SIZE))  # Resizes the Surface to a new size, given as (width, height)
+
+
 def drawGameState(screen, gs):
     colors = [p.Color("white"), p.Color("gray")]
     for r in range(DIMENSION):
         for c in range(DIMENSION):
             color = colors[(r + c) % 2]
-            p.draw.rect(screen, color, p.Rect(c * SQ_SIZE, r * SQ_SIZE, SQ_SIZE, SQ_SIZE)) #draw the rectangle
+            p.draw.rect(screen, color, p.Rect(c * SQ_SIZE, r * SQ_SIZE, SQ_SIZE, SQ_SIZE))  # draw the rectangle
             piece = gs.board[r][c]
             if (piece != '--'):
                 screen.blit(IMAGES[piece], p.Rect(c * SQ_SIZE, r * SQ_SIZE, SQ_SIZE, SQ_SIZE))
 
+
 '''MAIN DRIVER FOR CODE. UPDATING THE GRPHICS'''
+
+
 def main():
     # pygame setup
     p.init()
     screen = p.display.set_mode((WIDTH, HEIGHT))
-    clock = p.time.Clock() # doc (https://www.pygame.org/docs/ref/time.html)
+    clock = p.time.Clock()  # doc (https://www.pygame.org/docs/ref/time.html)
 
-    gs = GameState()  # game state
+    gs = Engine.GameState()  # game state
     loadImages()  # running this once before while loop
     running = True
 
+    sqSelected = ()  # no square is selected , keep track of the last click of the user (tuple : (row,col))
+    playercClicks = []  # keep track of player click (two tuples :[(6,4) , (4,4)])
     # poll for events
     while running:
         # pygame.QUIT event means the user clicked X to close your window
         for e in p.event.get():
             if e.type == p.QUIT:
                 running = False
+            elif e.type == p.MOUSEBUTTONDOWN:
+                location = p.mouse.get_pos()  # (x,y) is location of the mouse
+                col = (int)(location[0] // SQ_SIZE)
+                row = (int)(location[1] // SQ_SIZE)
+                if sqSelected == (row, col):  # the user clicked the same square twice
+                    sqSelected = ()  # deselect
+                    playercClicks = []  # clear player select
+                else:
+                    sqSelected = (row, col)
+                    playercClicks.append(sqSelected)  # append for both 1st and 2nd clicks
+                if len(playercClicks) == 2:  # after 2nd click
+                    move = Engine.Move(playercClicks[0], playercClicks[1], gs.board)
+                    print(move.getChessNotation())
+                    gs.makeMove(move)
+                    sqSelected = ()  # reset
+                    playercClicks = []  # reset
 
         # fill the screen with a color to wipe away anything from last frame
         screen.fill(p.Color("white"))
 
         # RENDER YOUR GAME HERE
         drawGameState(screen, gs)
-        clock.tick(MAX_FPS) #update the clock : never run at more than FPS
+        clock.tick(MAX_FPS)  # update the clock : never run at more than FPS
 
         # flip() the display to put your work on screen
         p.display.flip()
