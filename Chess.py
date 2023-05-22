18fe5f40c714f6a0bbd01cc703a240198bcde3b7
ThanhBear <leethanh24003@gmail.com>
2023-05-23 06:25:31 +0700

    update

--- Chess.py
+++ Chess.py
@@ -1,12 +1,27 @@
 """this is main driver file. It will be responsible for handling user input
 and displaying the current GameState object """
 import pygame as p
+import pygame.draw
+
 import Engine
 
-WIDTH = HEIGHT = 720
+p.init()
+clock = pygame.time.Clock()
+
+counter, text =10, '10'.rjust(3)
+tempCounter = counter
+timeTurn = 1 #white : 1 , black :0
+pygame.time.set_timer(pygame.USEREVENT, 1000)
+timeFont = pygame.font.SysFont('Consolas', 30)
+WIDTH = 800
+HEIGHT = 720
 DIMENSION = 8
-SQ_SIZE = HEIGHT / DIMENSION  # squre size
-MAX_FPS = 15
+SQ_SIZE = 75  # square size
+MAX_FPS = 60
+small_font = p.font.Font('freesansbold.ttf', 30)
+medium_font = p.font.Font('freesansbold.ttf', 40)
+big_font = p.font.Font('freesansbold.ttf', 50)
+turn_step = 0
 IMAGES = {}
 
 '''
@@ -20,9 +35,21 @@ def loadImages():
         IMAGES[piece] = p.transform.scale(p.image.load("images/" + piece + ".png"), (
             SQ_SIZE, SQ_SIZE))  # Resizes the Surface to a new size, given as (width, height)
 
+def timeCounter (timer ) :
+    x = timer #seconds
+    seconds = x % 60
+    minutes = int(x / 60) % 60
+
+    return f"{minutes:02}:{seconds:02}"
 
 def drawGameState(screen, gs):
     colors = [p.Color("white"), p.Color("gray")]
+    global counter, text ,timeTurn
+    counter -= 0.019
+
+    text = timeCounter(int(counter)).rjust(3)
+    if (counter ==0) :
+            return
     for r in range(DIMENSION):
         for c in range(DIMENSION):
             color = colors[(r + c) % 2]
@@ -32,12 +59,41 @@ def drawGameState(screen, gs):
             if (piece != '--'):
                 screen.blit(IMAGES[piece.name], p.Rect(c * SQ_SIZE, r * SQ_SIZE, SQ_SIZE, SQ_SIZE))
 
+    for i in range(9):
+        if i != 8:
+            pygame.draw.line(screen, 'black', (0, 75 * i), (75 * 8, 75 * i), 2)
+            pygame.draw.line(screen, 'black', (75 * i, 0), (75 * i, 75 * 8), 2)
+        else:
+            pygame.draw.line(screen, 'black', (75 * i, 0), (75 * i, WIDTH), 2)
+    if text != "00:00" :
+        if timeTurn ==1 :
+            screen.blit(big_font.render(f"White : {text}", True, 'black'), (20, 640))
+        else :
+
+            screen.blit(big_font.render(f"Black : {text}", True, 'black'), (20, 640))
+    else :
+        counter = tempCounter
+        if timeTurn ==1  :
+            timeTurn =0
+        else :
+            timeTurn =1
+
+
+
+    screen.blit(medium_font.render(" move", True, 'black'), (75*8+10, 0))
+    screen.blit(small_font.render(" Surrender", True, 'black'), (75*8+5, 75*8+30))
+    # screen.blit(small_font.render(text, True, 'black'), (75*8-200, 75*8+30))
+
+
+    pygame.draw.line(screen, 'black', (0, 75 * 8), (WIDTH, 75 * 8), 2)
+
     rK, cK = gs.teams[gs.player]['K'][0].position
     if gs.Check(rK, cK, gs.board) != []:
         s = p.Surface((SQ_SIZE, SQ_SIZE))
         s.set_alpha(100)
         s.fill(p.Color("red"))
         screen.blit(s, (cK * SQ_SIZE, rK * SQ_SIZE))
+
 
 def highlightSquare(screen, gs, validMoves, squareSelected):
     if squareSelected != ():
@@ -59,16 +115,14 @@ def highlightSquare(screen, gs, validMoves, squareSelected):
                 screen.blit(s, (move[1] * SQ_SIZE, move[0] * SQ_SIZE))
                 s.set_alpha(30)
                 s.fill(p.Color("blue"))
-                
-            
-                
+
 
 '''MAIN DRIVER FOR CODE. UPDATING THE GRPHICS'''
 
 
 def main():
     # pygame setup
-    p.init()
+
     screen = p.display.set_mode((WIDTH, HEIGHT))
     clock = p.time.Clock()  # doc (https://www.pygame.org/docs/ref/time.html)
 
@@ -97,7 +151,6 @@ def main():
                     sqSelected = (row, col)
                     playercClicks.append(sqSelected)  # append for both 1st and 2nd clicks
 
-
             if end == 0 and len(playercClicks) == 2:  # after 2nd click
                 move = Engine.Move(playercClicks[0], playercClicks[1], gs.board)
                 gs.makeMove(move)
@@ -107,18 +160,16 @@ def main():
                     break
                 sqSelected = ()  # reset
                 playercClicks = []  # reset
-            
-
 
         # fill the screen with a color to wipe away anything from last frame
-        screen.fill(p.Color("white"))
+        screen.fill(p.Color("gray"))
 
         # RENDER YOUR GAME HERE
         drawGameState(screen, gs)
-        
+
         if gs.board[row][col] != '--':
             validMoves = gs.board[row][col].getAllValidMoves(gs)
-            
+
             highlightSquare(screen, gs, validMoves, sqSelected)
 
         clock.tick(MAX_FPS)  # update the clock : never run at more than FPS
--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import pygame, sys
 
-import Main
+import Chess
 from button import Button
 
 pygame.init()
 
-SCREEN = pygame.display.set_mode((720, 720))
+WIDTH = 800
+HEIGHT = 720
+
+SCREEN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Menu")
 
 BG = pygame.image.load("images/chessBackground2.jpg")
@@ -72,13 +75,13 @@ def main_menu():
         MENU_MOUSE_POS = pygame.mouse.get_pos()
 
         MENU_TEXT = get_font(70).render("MAIN MENU", True, "#b68f40")
-        MENU_RECT = MENU_TEXT.get_rect(center=(720/2, 120))
+        MENU_RECT = MENU_TEXT.get_rect(center=(WIDTH/2, 120))
 
-        PLAY_BUTTON = Button(image=pygame.image.load("images/Play Rect.png"), pos=(720/2, 250),
+        PLAY_BUTTON = Button(image=pygame.image.load("images/Play Rect.png"), pos=(WIDTH/2, 250),
                              text_input="PLAY", font=get_font(40), base_color="#d7fcd4", hovering_color="White")
-        OPTIONS_BUTTON = Button(image=pygame.image.load("images/Play Rect.png"), pos=(720/2, 400),
+        OPTIONS_BUTTON = Button(image=pygame.image.load("images/Play Rect.png"), pos=(WIDTH/2, 400),
                                 text_input="OPTIONS", font=get_font(40), base_color="#d7fcd4", hovering_color="White")
-        QUIT_BUTTON = Button(image=pygame.image.load("images/Play Rect.png"), pos=(720/2, 550),
+        QUIT_BUTTON = Button(image=pygame.image.load("images/Play Rect.png"), pos=(WIDTH/2, 550),
                              text_input="QUIT", font=get_font(40), base_color="#d7fcd4", hovering_color="White")
 
         SCREEN.blit(MENU_TEXT, MENU_RECT)
@@ -94,7 +97,7 @@ def main_menu():
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if PLAY_BUTTON.checkForInput(MENU_MOUSE_POS):
                     #play()
-                    Main.main()
+                    Chess.main()
                     return
                 if OPTIONS_BUTTON.checkForInput(MENU_MOUSE_POS):
                     options()
